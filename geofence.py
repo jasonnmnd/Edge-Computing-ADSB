d3e567db3ed58eca07a3c27f989ec15bffa9680d
jasonnmnd <85291489+jasonnmnd@users.noreply.github.com>
2022-02-27 15:55:50 -0500

    changed LED

--- geofence.py
+++ geofence.py
@@ -10,20 +10,26 @@ def setupGeofence(planes):
         for radius in plane.radius:
             if radius <= 20:
                 print("Plane within 20 miles")
+                greenLED.off()
+                yellowLED.off()
                 redLED.on()
-                time.sleep(10)
+                time.sleep(5)
                 break
 
             if radius >20 and radius <= 50:
                 print("Plane within 20 and 50 miles")
+                redLED.off()
+                greenLED.off()
                 yellowLED.on()
-                time.sleep(10)
+                time.sleep(5)
                 break
 
             if radius > 50 and radius <=100:
                 print("Plane within 50 and 100 miles")
+                redLED.off()
+                yellowLED.off()
                 greenLED.on()
-                time.sleep(10)
+                time.sleep(5)
                 break
 
 if __name__ == "__main__":
